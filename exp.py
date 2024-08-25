73cc6c468647ea5b0ec740b6d310f4648c658a31
SinghAman21 <singhaman1825@gmail.com>
2024-08-25 20:32:57 +0530

    New PATH added

---
+++ exp.py
@@ -0,0 +1,20 @@
+# <<<<<<<<<<<<<<  ✨ Codeium Command 🌟  >>>>>>>>>>>>>>>>
+import os
+
+
+
+path = os.environ.get("PATH")
+
+PATH = os.environ.get("PATH")
+# print("Current Directory:", current_directory)
+print("PATH:", PATH)
+# print("\n")
+
+current_directory = os.getcwd()
+print("\n")
+print("\n")
+
+
+print("Current Directory:", current_directory)
+# print("PATH:", path)
+# <<<<<<<  47c7d28d-b2d9-4253-b53a-6951f5b4b021  >>>>>>>
